429303ffe017daabf31fad7f54ff9b6a4aeab2c6
Mostafa Allam <mostafaallam602@gmail.com>
2022-04-17 18:23:31 +0200

    ch6

---
+++ users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import generic 
+from django.urls import reverse_lazy
+
+from .forms import CustomUserCreationForm 
+
+
+
+class SignupPageView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
