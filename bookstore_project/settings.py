68f8e1c0d398c46ecfbbcf24562b62cbe3e601b7
Mostafa Allam <mostafaallam602@gmail.com>
2022-04-25 23:49:22 +0200

    ch8 Adding environmet variables configrations

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*@e^!9g%8+pr&g@m^##&_5+_optmgm#(cqiob=thmm66n%dxy%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.eviron.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = ['*']
 
