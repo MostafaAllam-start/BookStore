4c6dccccfd1b9140c88d93119fc158aa0f6a28b7
Mostafa Allam <mostafaallam602@gmail.com>
2022-04-29 13:34:46 +0200

    ch8 sending email and authentication using email only

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.eviron.get('DEBUG', default=0))
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = ['*']
 
@@ -163,3 +163,6 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
+
+
+DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
