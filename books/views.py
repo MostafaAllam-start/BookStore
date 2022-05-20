facc9acee3ad389ea5e6a0b105289c72277dd4cd
Mostafa Allam <mostafaallam602@gmail.com>
2022-05-20 06:52:25 +0200

    ch10 adding the Book list and detail pages and configur the uuid in our book model

---
+++ books/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Book
+from django.views.generic import ListView, DetailView
+
+class BookListView(ListView):
+    model= Book
+    template_name = 'books/book_list.html'
+    context_object_name = 'book_list'
+
+class BookDetailView(DetailView):
+    model = Book
+    context_object_name = 'book_detail'
+    template_name = 'books/book_detail.html'
--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'books.apps.BooksConfig',
 ]
 
 MIDDLEWARE = [
@@ -133,7 +134,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'users.CustomUser' 
+# AUTH_USER_MODEL = 'users.CustomUser' 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
